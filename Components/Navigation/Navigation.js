7b6d8179f92a3d558e7a7900abdecf172dd4be55
blacol <anthonycolvil@gmail.com>
2021-06-21 09:00:31 +0200

    a finir

--- Components/Navigation/Navigation.js
+++ Components/Navigation/Navigation.js
@@ -1,20 +1,42 @@
+// In App.js in a new project
+
 import * as React from 'react';
+import { View, Text,Buttonii" } from 'react-native';
+import Accueil from '../Page/Accueil';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+
 const Stack = createStackNavigator();
 
-const MyStack = () => {
+function Navigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: 'Welcome' }}
-        />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-};
+}
+
+export default Navigation;
