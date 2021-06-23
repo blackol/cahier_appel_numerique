1b04eefccc70decc39099fd4c577d0dcb8ccb738
blacol <anthonycolvil@gmail.com>
2021-06-23 13:58:30 +0200

    flexbox

--- Components/Page/Accueil.js
+++ Components/Page/Accueil.js
@@ -14,26 +14,65 @@ export default class Accueil extends React.Component {
     }
     render() {
         return (
-            <View style={styles.container}>
-                <Image source={require('../asset/Fouillole.jpg')}  style={{ width: 60, height: 60 }}/> 
-                <TextInput
-                value={this.state.id_student}
-                onChangeText={(id_student) => this.setState({id_student})}
-                placeholder='Numéro Étudiant'
-                style={styles.input}
-                />
-                 <TextInput
-                value={this.state.password}
-                onChangeText={(password) => this.setState({password})}
-                placeholder='Password'
-                style={styles.input}
-                />
-                <Button
-                title="Connecte-toi"
-                color="#0074b7"
-                onPress={() => this.props.navigation.navigate("Details")}
-                />
+          <View style={styles.container}>
+            <View style={{ flex: 3 }} >
+              
+                <Image source={require('../asset/Fouillole.jpg')}  style={{ width: 250, height: 250 }}/>
+              
             </View>
+            
+            
+            <View style={{ flex: 2 }}>
+            <TextInput
+                  value={this.state.id_student}
+                  onChangeText={(id_student) => this.setState({id_student})}
+                  placeholder='Numéro Étudiant'
+                  style={styles.input}
+                  />
+                  <TextInput
+                  value={this.state.password}
+                  onChangeText={(password) => this.setState({password})}
+                  placeholder='Password'
+                  type='password'
+                  style={styles.input}
+                  />
+            </View>
+            <View style={{ flex: 3 }}>
+            <Button
+                style={styles.buttonStyles}
+                  title="Connecte-toi"
+                  color="#0074b7"
+                  onPress={() => this.props.navigation.navigate("Details")}
+                  />
+            </View>
+          </View>
+
+
+          
+          /*
+            <View style={styles.container}>
+              <View style={styles.ConteneurButton}>
+                <Image source={require('../asset/Fouillole.jpg')}  style={{ width: 60, height: 60 }}/> 
+              </View>
+              <View style={styles.ConteneurButton}>
+                <TextInput
+                  value={this.state.id_student}
+                  onChangeText={(id_student) => this.setState({id_student})}
+                  placeholder='Numéro Étudiant'
+                  style={styles.input}
+                  />
+                  <TextInput
+                  value={this.state.password}
+                  onChangeText={(password) => this.setState({password})}
+                  placeholder='Password'
+                  style={styles.input}
+                  />
+              </View>
+              <View style={styles.ConteneurButton}>
+                
+              </View>
+            </View>
+            */
             
         );
     }
@@ -42,14 +81,13 @@ export default class Accueil extends React.Component {
 
 
 const styles = StyleSheet.create({
-  container: {
+  container: 
+  {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection:"column",
   },
   input: {
-    width: 200,
+    //width: 200,
     height: 40,
     margin: 12,
     borderColor: 'black',
@@ -65,6 +103,18 @@ const styles = StyleSheet.create({
     padding: 5,
     width : 150,
     borderRadius: 25,
+  },
+  Conteneurphoto :{
+    flex: 1,
+    backgroundColor:'red',
+  },
+  ConteneurInput :{
+    flex: 1,
+    backgroundColor:'yellow',
+  },
+  ConteneurButton :{
+    flex: 1,
+    backgroundColor:'green',
   },
 
 
