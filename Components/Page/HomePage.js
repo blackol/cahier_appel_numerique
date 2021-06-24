efa0be24eb88b880a8d27c64111ae2e2934c40af
blacol <anthonycolvil@gmail.com>
2021-06-24 13:38:48 +0200

    Integration Drawer navigation

---
+++ Components/Page/HomePage.js
@@ -0,0 +1,38 @@
+import { Assets } from '@react-navigation/stack';
+import React from 'react';
+import { StyleSheet, Text, View,Button,TextInput,onChangeText,Image, Alert, TouchableOpacity} from 'react-native';
+
+
+export default class HomePage extends React.Component {
+    
+    
+    ///////////////////////:::esijbpiubqpidiudb
+    render() {
+        return (
+          <View style={styles.container}>
+            <View></View>
+            <View></View>
+          </View>
+
+
+          
+          
+            
+        );
+    }
+    
+}
+
+
+const styles = StyleSheet.create({
+  container: 
+  {
+    flex: 1,
+    flexDirection:"column",
+    marginTop:30,
+   
+  },
+
+  
+
+});
