ecee7987eeb18499f2424194e7aab3f85ae8459b
LIPAU <44001700+LIPAU@users.noreply.github.com>
2021-07-07 20:24:27 -0400

    Update PageDetails.js

--- Components/Page/PageDetails.js
+++ Components/Page/PageDetails.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View,Button } from 'react-native';
 
+
 export default class PageDetails extends React.Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      active:true,
+    }
+  }
 
     render() {
         return (
             <View style={styles.container}>
-                <Text>Page details</Text>  
+                <Text>Page detail</Text>
+                <Button
+                  title="Connecte-toi"
+                  color="#0074b7"
+                  onPress={() => this.props.navigation.navigate("Accueil")}
+                  />
             </View>
         );
     }
